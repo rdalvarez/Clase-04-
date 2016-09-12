779e0b8117191fd729226accc5c3d10bb62a82ee
rdalvarez <david_de_odin@hotmail.com>
2016-09-12 20:41:36 -0300

    leemos el archivo

--- nexo.php
+++ nexo.php
@@ -1,13 +1,17 @@
 <?php 
 include_once ("php/Estacionamiento.php");
 
-	var_dump($_POST);
-	echo "<br>";	
+var_dump($_POST);
+echo "<br><br>";	
+/*
+if (isset($_POST['Ingresar'])) {
 
-	if (isset($_POST['Ingresar'])) {
+	$aux=$_POST['txtIngreso'];
+	Estacionamiento::Guardar($aux);	
+}*/
 
-		$aux=$_POST['txtIngreso'];
-		Estacionamiento::Guardar($aux);	
-	}
+$milistado = Estacionamiento::Leer();
+var_dump($milistado);
 	
- ?>
+echo '<p><a href="index.php">Volver</a></p>';
+?>
--- php/Estacionamiento.php
+++ php/Estacionamiento.php
@@ -11,11 +11,34 @@ class Estacionamiento
 	{
 		$archivo = fopen("Estacionados.txt", "a"); //creo el archivo
 
-		fwrite($archivo, $patente); //escribo el archivo
+		$ahora = date("Y-m-d H-i-s");
+
+		$renglon = $patente . "=>" . $ahora . "\n";
+
+		fwrite($archivo, $renglon); //escribo el archivo
 
 		fclose($archivo); //cierro el archivo
 
 		echo "Esta guardado";
 	}
+
+	public static function Leer()
+	{
+		$listadoDeAutos = array(); //creamos un array
+
+		//$listaDeAutosLeida[] = "Primer Elemento"; //manera de agregar un elemento
+
+		$archivo = fopen("Estacionados.txt", "r");
+
+		while (!feof($archivo)) { //hasta que no este en el ultimo renglon del archivo
+			$renglon = fgets($archivo); //saco el renglon
+
+			$listadoDeAutos[] = $renglon; //lo paso al string
+		}
+
+		fclose($archivo);
+
+		return $listadoDeAutos;
+	} 
 }
 ?>
