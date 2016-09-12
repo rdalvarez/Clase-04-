2aaf65912bd314ef02a4414875bcea1917137631
rdalvarez <david_de_odin@hotmail.com>
2016-09-12 19:37:59 -0300

    Crear un formulario con 1 text y 2 botones, que llegue la informacion a nexo.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+<div class="CajaUno">
+	<form action="nexo.php" method="POST" id="FrmInicio">
+
+		<input type="text" name="txtIngreso" placeholder="Ingrese Datos">
+
+		<input type="submit" name="btnUno" value="Boton 1" class="MiBotonUTNMenuInicio">
+		<input type="submit" name="btnDos" value="Boton 2" class="MiBotonUTNMenuInicio">
+
+	</form>
+</div>
+</body>
+</html>
